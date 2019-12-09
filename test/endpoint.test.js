27b47e0a5f130da55a8fc6c946bc40ee63b17dd0
Mikkel <MikkelHansen95@live.dk>
2019-12-09 12:19:26 +0100

    tests added

--- test/endpoint.test.js
+++ test/endpoint.test.js
@@ -1,12 +1,31 @@
-var request = require('request');
+const fetch = require('node-fetch');
 
 
 test('get all caterings', async () => {
-    
-    let res  = await request.get('http://localhost:3000/catering')
-    .on('response',function(response){
-        console.log(response.body)
-        expect(3).toBe(3);
-    })
-    //console.log(res)
-  });
+
+    return fetch('http://localhost:3000/catering')
+        .then(res => {
+            return res.json()
+        })
+        .then(res => {
+            return expect(res.length).toBe(5);
+        })
+        .catch(e => {
+            console.log(e)
+        })
+});
+
+
+test('check if catering on "JustasStreet" exists', async () => {
+
+    return fetch('http://localhost:3000/catering')
+        .then(res => {
+            return res.json()
+        })
+        .then(res => {
+            return expect(res[0].address).toBe("JustasStreet");
+        })
+        .catch(e => {
+            console.log(e)
+        })
+});
